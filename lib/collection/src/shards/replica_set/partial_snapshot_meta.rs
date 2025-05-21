98de210becb11c70d27672d77074949a00f666fd
Roman Titov <ffuugoo@users.noreply.github.com>
2025-05-21 11:41:18 +0200

    Add partial snapshot requests tracker and recovery timestamp to telemetry

--- lib/collection/src/shards/replica_set/partial_snapshot_meta.rs
+++ lib/collection/src/shards/replica_set/partial_snapshot_meta.rs
@@ -1,29 +1,38 @@
 use std::sync::Arc;
+use std::sync::atomic::{self, AtomicU64, AtomicUsize};
+use std::time::{Duration, SystemTime};
 
 use crate::operations::types::{CollectionError, CollectionResult};
 
 #[derive(Debug)]
 pub struct PartialSnapshotMeta {
+    ongoing_create_snapshot_requests_tracker: RequestTracker,
     recovery_lock: Arc<tokio::sync::Mutex<()>>,
     read_lock: Arc<tokio::sync::Semaphore>,
+    recovery_timestamp: AtomicU64,
 }
 
 impl PartialSnapshotMeta {
     pub fn new() -> Self {
         Self {
+            ongoing_create_snapshot_requests_tracker: RequestTracker::new(),
             recovery_lock: Arc::new(tokio::sync::Mutex::new(())),
             read_lock: Arc::new(tokio::sync::Semaphore::new(1)),
+            recovery_timestamp: AtomicU64::new(0),
         }
+    }
+
+    pub fn ongoing_create_snapshot_requests(&self) -> usize {
+        self.ongoing_create_snapshot_requests_tracker.requests()
+    }
+
+    pub fn track_create_snapshot_request(&self) -> RequestGuard {
+        self.ongoing_create_snapshot_requests_tracker
+            .track_request()
     }
 
     pub fn take_recovery_lock(&self) -> CollectionResult<tokio::sync::OwnedMutexGuard<()>> {
         self.recovery_lock.clone().try_lock_owned().map_err(|_| {
-            CollectionError::bad_request("partial snapshot recovery is already in progress")
-        })
-    }
-
-    pub fn take_read_lock(&self) -> CollectionResult<tokio::sync::OwnedSemaphorePermit> {
-        self.read_lock.clone().try_acquire_owned().map_err(|_| {
             CollectionError::bad_request("partial snapshot recovery is already in progress")
         })
     }
@@ -39,5 +48,62 @@ impl PartialSnapshotMeta {
                 backtrace: None,
             })
         }
+    }
+
+    pub fn take_read_lock(&self) -> CollectionResult<tokio::sync::OwnedSemaphorePermit> {
+        self.read_lock.clone().try_acquire_owned().map_err(|_| {
+            CollectionError::bad_request("partial snapshot recovery is already in progress")
+        })
+    }
+
+    pub fn recovery_timestamp(&self) -> u64 {
+        self.recovery_timestamp.load(atomic::Ordering::Relaxed)
+    }
+
+    pub fn snapshot_recovered(&self) {
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap_or(Duration::ZERO)
+            .as_secs();
+
+        self.recovery_timestamp
+            .store(timestamp, atomic::Ordering::Relaxed);
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct RequestTracker {
+    requests: Arc<AtomicUsize>,
+}
+
+impl RequestTracker {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn requests(&self) -> usize {
+        self.requests.load(atomic::Ordering::Relaxed)
+    }
+
+    pub fn track_request(&self) -> RequestGuard {
+        RequestGuard::new(self.requests.clone())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct RequestGuard {
+    requests: Arc<AtomicUsize>,
+}
+
+impl RequestGuard {
+    fn new(requests: Arc<AtomicUsize>) -> Self {
+        requests.fetch_add(1, atomic::Ordering::Relaxed);
+        Self { requests }
+    }
+}
+
+impl Drop for RequestGuard {
+    fn drop(&mut self) {
+        self.requests.fetch_sub(1, atomic::Ordering::Relaxed);
     }
 }
--- lib/collection/src/shards/replica_set/telemetry.rs
+++ lib/collection/src/shards/replica_set/telemetry.rs
@@ -27,6 +27,10 @@ impl ShardReplicaSet {
                 .map(|remote| remote.get_telemetry_data(detail))
                 .collect(),
             replicate_states: self.replica_state.read().peers(),
+            ongoing_create_partial_snapshot_requests: self
+                .partial_snapshot_meta
+                .ongoing_create_snapshot_requests(),
+            partial_snapshot_recovery_timestamp: self.partial_snapshot_meta.recovery_timestamp(),
         }
     }
 
